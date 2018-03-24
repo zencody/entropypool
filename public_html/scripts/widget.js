265ccd8e2f789e7c90eeb468e902f0b347aa16f0
cody <thecodster@gmail.com>
2018-03-24 09:04:58 -0700

    add widget / html to index

---
+++ public_html/scripts/widget.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+window.zESettings = {
+  webWidget: {
+    contactOptions: {
+      enabled: true,
+    }
+    color: { theme: '#9fedd7' }
+  }
+};
+</script>
+
+
+<!-- Start of z3ncody Zendesk Widget script -->
+<script>/*<![CDATA[*/window.zE||(function(e,t,s){var n=window.zE=window.zEmbed=function(){n._.push(arguments)}, a=n.s=e.createElement(t),r=e.getElementsByTagName(t)[0];n.set=function(e){ n.set._.push(e)},n._=[],n.set._=[],a.async=true,a.setAttribute("charset","utf-8"), a.src="https://static.zdassets.com/ekr/asset_composer.js?key="+s, n.t=+new Date,a.type="text/javascript",r.parentNode.insertBefore(a,r)})(document,"script","2aa235c1-68b6-4ddb-a7fe-903f2550481e");/*]]>*/</script>
+<!-- End of z3ncody Zendesk Widget script -->
